fce306c7972b0531a5047f4d72bb61fdbbd423c5
lehuong099-QA <huong.le2@niteco.se>
2023-08-15 23:09:39 +0700

    hide redundant line

--- Cypress/cypress/Pages/PDP1.js
+++ Cypress/cypress/Pages/PDP1.js
@@ -9,17 +9,17 @@ class PdpPage {
         addToCartbtn:() => cy.get('.pdp-info__btn > .btn'),
         itemPriceInMiniCart:() => cy.get('.mini-cart__product-price--cost'),
         qtyinMiniCart:()=>cy.get('.mini-cart__product-quantity > .input'),
-        proceedToCheckOutbtn: () => cy.get(':nth-child(2) > .mini-cart__btn-to-checkout'),
-        // login via login popup
-        usernametxt1: () =>cy.get('#dialog-checkout > .bn-dialog-container > [dropdown-panel-container=""] > .bn-dialog__body > .login > .login__right > .login__right__wrapper > .login__auth > .login__form > :nth-child(1) > .input'),
-        Passwordtxt1: () => cy.get('#dialog-checkout > .bn-dialog-container > [dropdown-panel-container=""] > .bn-dialog__body > .login > .login__right > .login__right__wrapper > .login__auth > .login__form > :nth-child(2) > .input'),
-        signinbtn1:() => cy.get('#dialog-checkout > .bn-dialog-container > [dropdown-panel-container=""] > .bn-dialog__body > .login > .login__right > .login__right__wrapper > .login__auth > .login__form > .btn'),
-        // login via login page
-        myaccounticon:() => cy.get('.header__util-account-btn > .btn-flat'),
-        loginicon:() => cy.get('.has-prev-url > a'),
-        usernametxt: () => cy.get('.login__form > :nth-child(1) > .input'),
-        Passwordtxt:() => cy.get('.login__form > :nth-child(2) > .input'),
-        signinbtn: () => cy.get('.login__form > .btn')
+        // proceedToCheckOutbtn: () => cy.get(':nth-child(2) > .mini-cart__btn-to-checkout'),
+        // // login via login popup
+        // usernametxt1: () =>cy.get('#dialog-checkout > .bn-dialog-container > [dropdown-panel-container=""] > .bn-dialog__body > .login > .login__right > .login__right__wrapper > .login__auth > .login__form > :nth-child(1) > .input'),
+        // Passwordtxt1: () => cy.get('#dialog-checkout > .bn-dialog-container > [dropdown-panel-container=""] > .bn-dialog__body > .login > .login__right > .login__right__wrapper > .login__auth > .login__form > :nth-child(2) > .input'),
+        // signinbtn1:() => cy.get('#dialog-checkout > .bn-dialog-container > [dropdown-panel-container=""] > .bn-dialog__body > .login > .login__right > .login__right__wrapper > .login__auth > .login__form > .btn'),
+        // // login via login page
+        // myaccounticon:() => cy.get('.header__util-account-btn > .btn-flat'),
+        // loginicon:() => cy.get('.has-prev-url > a'),
+        // usernametxt: () => cy.get('.login__form > :nth-child(1) > .input'),
+        // Passwordtxt:() => cy.get('.login__form > :nth-child(2) > .input'),
+        // signinbtn: () => cy.get('.login__form > .btn')
     }
     OpenLoginpage()
     {
